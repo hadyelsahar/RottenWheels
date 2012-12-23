b8292a27113e0839caeb33ce30bb312a9179d6e0
saasbook <saasbook@saasbook.(none)>
2012-12-23 03:27:54 +0200

    scaffold for cars was generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RottenWheels::Application.config.secret_token = 'f3dfd3b1b0cf7f42b5b37a4598303282be3186d5ed63548e0ef924e04beab88062b223c7ff90859fa5644f0ad5befb0bf115614fdb850fafb8eafc949f7fec47'
